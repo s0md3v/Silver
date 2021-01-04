a2ac2865e937acfb44fe0acfdab80eac94d3339e
Somdev Sangwan <s0md3v@gmail.com>
2021-01-04 20:17:08 +0530

    fixes #9, skip host discovery

--- modules/pymap.py
+++ modules/pymap.py
@@ -3,15 +3,13 @@ import subprocess
 
 from core.utils import load_json
 
-def pymap(ip, ports, exclude):
-	if ip not in exclude:
-		result = {}
-		result[ip] = {}
-		ports = ','.join([str(port) for port in ports])
-		info = subprocess.getoutput('nmap -oX - -sV %s -p%s' % (ip, ports))
-		matches = re.finditer(r'<port protocol="\w+" portid="(\d+)"><state state="([^"]+)" reason="[^"]+" reason_ttl="\d+"/><service name="([^"]*)"( product="([^"]*)")?( version="([^"]*)")?.*?>.*?(<cpe>([^<]*))?', info)
-		if matches:
-			for match in matches:
+def parse_result(ip, nmapfile):
+	result = {}
+	result[ip] = {}
+	with open(nmapfile, 'r') as file:
+		for line in file:
+			match = re.search(r'<port protocol="\w+" portid="(\d+)"><state state="([^"]+)" reason="[^"]+" reason_ttl="\d+"/><service name="([^"]*)"( product="([^"]*)")?( version="([^"]*)")?.*?>.*?(<cpe>([^<]*))?', line)
+			if match:
 				port = match.group(1)
 				result[ip][port] = {}
 				result[ip][port]['state'] = match.group(2)
@@ -22,6 +20,12 @@ def pymap(ip, ports, exclude):
 				if not match.group(7) and match.group(9):
 					if match.group(9).count(':') > 3:
 						result[ip][port]['version'] = match.group(9).split(':')[-1]
-		return result
+	return result
+
+def pymap(ip, ports, exclude, nmapfile):
+	if ip not in exclude:
+		ports = ','.join([str(port) for port in ports])
+		subprocess.getoutput('nmap -Pn -oX %s -sV %s -p%s' % (nmapfile, ip, ports))
+		return parse_result(ip, nmapfile)
 	else:
 		return 'cached'
