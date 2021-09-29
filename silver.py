bd4567475e363bc2103cb176aaa7bb3c78b148be
Alessio DS <digialessio63@gmail.com>
2021-09-29 22:30:44 +0200

    Uncompromising formatting.

--- silver.py
+++ silver.py
@@ -16,161 +16,189 @@ from core.resolver import resolver
 from core.colors import run, bad, end, good, info, white
 from core.utils import notify, load_json, write_json, parse_masscan
 
-print('''
+print(
+    """
 \t%s𝘴𝘪𝘭𝘷𝘦𝘳%s
-''' % (white, end))
+"""
+    % (white, end)
+)
 
 if os.geteuid() != 0:
-	quit('%s You need to run Silver a root!' % info)
+    quit("%s You need to run Silver a root!" % info)
 
 cwd = sys.path[0]
 
 parser = argparse.ArgumentParser()
-parser.add_argument(help='host(s) to scan', dest='host', nargs='?')
-parser.add_argument('-i', help='path of input file', dest='input_file')
-parser.add_argument('-m', '--method', help='software or cpe', dest='method')
-parser.add_argument('-r', '--rate', help='masscan packets per second rate', dest='rate', type=int, default=1000)
-parser.add_argument('-t', '--threads', help='nmap threads to run in parallel', dest='threads', type=int)
-parser.add_argument('-q', '--quick', help='only scan top ~1000 ports', dest='quick', action='store_true')
-parser.add_argument('--resolve', help='txt file contains hostnames too', dest='resolve', action='store_true')
+parser.add_argument(help="host(s) to scan", dest="host", nargs="?")
+parser.add_argument("-i", help="path of input file", dest="input_file")
+parser.add_argument("-m", "--method", help="software or cpe", dest="method")
+parser.add_argument(
+    "-r",
+    "--rate",
+    help="masscan packets per second rate",
+    dest="rate",
+    type=int,
+    default=1000,
+)
+parser.add_argument(
+    "-t", "--threads", help="nmap threads to run in parallel", dest="threads", type=int
+)
+parser.add_argument(
+    "-q", "--quick", help="only scan top ~1000 ports", dest="quick", action="store_true"
+)
+parser.add_argument(
+    "--resolve",
+    help="txt file contains hostnames too",
+    dest="resolve",
+    action="store_true",
+)
 args = parser.parse_args()
 
-host = ','.join(resolver(args.host.split(','))) if args.host else args.host
+host = ",".join(resolver(args.host.split(","))) if args.host else args.host
 quick = args.quick
 method = args.method
 threads = args.threads
 input_file = args.input_file
 
-target_name = ''
+target_name = ""
 
 if host:
-	target_name = host.strip().split(',')[0].split('/')[0]
+    target_name = host.strip().split(",")[0].split("/")[0]
 elif input_file:
-	target_name = input_file.split('/')[-1]
+    target_name = input_file.split("/")[-1]
 else:
-	quit('%s No hosts to scan.' % bad)
+    quit("%s No hosts to scan." % bad)
 
-savefile = cwd + '/result-' + target_name + '.txt'
-nmapfile = cwd + '/nmap-' + target_name + '.xml'
+savefile = cwd + "/result-" + target_name + ".txt"
+nmapfile = cwd + "/nmap-" + target_name + ".xml"
 
 if args.resolve and input_file:
-	print('%s Resolving hostnames to IPs for masscan' % run)
-	input_file = resolver(input_file)
+    print("%s Resolving hostnames to IPs for masscan" % run)
+    input_file = resolver(input_file)
 
 arg_dict = vars(args)
 for key in arg_dict:
-	core.memory.global_vars[key] = arg_dict[key]
-core.memory.global_vars['shodan_queries'] = 0
+    core.memory.global_vars[key] = arg_dict[key]
+core.memory.global_vars["shodan_queries"] = 0
 
-hostfile = ''
+hostfile = ""
 if not host:
-	host = ' '
-	hostfile = '-iL ' + input_file
+    host = " "
+    hostfile = "-iL " + input_file
 else:
-	host = ' %s ' % host
+    host = " %s " % host
 
 use_cpe = True
-if method == 'software':
-	use_cpe = False
+if method == "software":
+    use_cpe = False
 
-ports_to_scan = '0-65535'
+ports_to_scan = "0-65535"
 if quick:
-	ports_to_scan = ','.join(core.memory.config['top_ports'])
+    ports_to_scan = ",".join(core.memory.config["top_ports"])
 
 database = {}
 
-print('%s Deploying masscan' % run)
+print("%s Deploying masscan" % run)
 masscan_saved = load_json(savefile)
 
 if not masscan_saved:
-	file = open(savefile, 'w+')
-	file.close()
+    file = open(savefile, "w+")
+    file.close()
 
 exclude = [host for host in masscan_saved]
 if exclude:
-	exclude = ' --exclude ' + ','.join(exclude) + ' '
+    exclude = " --exclude " + ",".join(exclude) + " "
 else:
-	exclude = ''
-os.system('masscan%s-p%s --rate %i -oG %s %s %s >/dev/null 2>&1' % (host, ports_to_scan, args.rate, savefile, hostfile, exclude))
+    exclude = ""
+os.system(
+    "masscan%s-p%s --rate %i -oG %s %s %s >/dev/null 2>&1"
+    % (host, ports_to_scan, args.rate, savefile, hostfile, exclude)
+)
 master_db = parse_masscan(savefile)
 for host in masscan_saved:
-	master_db[host] = masscan_saved[host]
+    master_db[host] = masscan_saved[host]
 write_json(savefile, master_db)
-print('%s Result saved to %s' % (info, savefile))
+print("%s Result saved to %s" % (info, savefile))
 
 exclude = []
 cached_hosts = load_json(savefile)
 for host in cached_hosts:
-	for port in cached_hosts[host]:
-		if 'software' in cached_hosts[host][port]:
-			exclude.append(host)
-		break
+    for port in cached_hosts[host]:
+        if "software" in cached_hosts[host][port]:
+            exclude.append(host)
+        break
 
 count = 0
-shodan_count = core.memory.config['max_shodan_credits'] * 20
+shodan_count = core.memory.config["max_shodan_credits"] * 20
 shodan_eligible = []
 for host in master_db:
-	if host not in exclude:
-		if len(master_db[host]) >= core.memory.config['shodan_call_threshold'] and core.memory.config['shodan_api_key']:
-			shodan_eligible.append(host)
-			shodan_count -= 1
-		for port in master_db[host]:
-			count += 1
+    if host not in exclude:
+        if (
+            len(master_db[host]) >= core.memory.config["shodan_call_threshold"]
+            and core.memory.config["shodan_api_key"]
+        ):
+            shodan_eligible.append(host)
+            shodan_count -= 1
+        for port in master_db[host]:
+            count += 1
 
-print('%s %i services to fingerprint' % (run, count))
+print("%s %i services to fingerprint" % (run, count))
 
 if shodan_eligible:
-	for host in shodan_eligible:
-		result = shodan(host)
-		for port in result:
-			master_db[port] = result[port]
-		write_json(savefile, master_db)
-	exclude = []
-	for host in master_db:
-		for port in master_db[host]:
-			if 'software' in master_db[host][port]:
-				exclude.append(host)
-			break
+    for host in shodan_eligible:
+        result = shodan(host)
+        for port in result:
+            master_db[port] = result[port]
+        write_json(savefile, master_db)
+    exclude = []
+    for host in master_db:
+        for port in master_db[host]:
+            if "software" in master_db[host][port]:
+                exclude.append(host)
+            break
 
 num_cpus = threads or psutil.cpu_count()
 if num_cpus > len(master_db):
-	num_cpus = len(master_db)
+    num_cpus = len(master_db)
 
 if num_cpus > 1:
-	print('%s Spawning %i nmap instances in parallel' % (run, num_cpus))
+    print("%s Spawning %i nmap instances in parallel" % (run, num_cpus))
 else:
-	print('%s Spawning 1 nmap instance' % run)
+    print("%s Spawning 1 nmap instance" % run)
 
 if num_cpus != 0:
-	print('%s ETA: %i seconds ' % (info, count * 22/num_cpus))
-	pool = Pool(processes=num_cpus)
+    print("%s ETA: %i seconds " % (info, count * 22 / num_cpus))
+    pool = Pool(processes=num_cpus)
 
-	results = [pool.apply_async(pymap, args=(host, master_db[host], exclude, nmapfile)) for host in master_db]
+    results = [
+        pool.apply_async(pymap, args=(host, master_db[host], exclude, nmapfile))
+        for host in master_db
+    ]
 
-	for p in results:
-		result = p.get()
-		if result == 'cached':
-			continue
-		for host in result:
-			master_db[host] = result[host]
-			write_json(savefile, master_db)
+    for p in results:
+        result = p.get()
+        if result == "cached":
+            continue
+        for host in result:
+            master_db[host] = result[host]
+            write_json(savefile, master_db)
 
-print('%s Updated %s' % (info, savefile))
+print("%s Updated %s" % (info, savefile))
 
-print('%s Looking for vulnerablilites' % run)
+print("%s Looking for vulnerablilites" % run)
 
 for ip in master_db:
-	for port in master_db[ip]:
-		cpe = master_db[ip][port]['cpe']
-		name = master_db[ip][port]['software']
-		version = master_db[ip][port]['version']
-		software = name
-		if use_cpe:
-			software = cpe
-		is_vuln = vulners(software, version, cpe=use_cpe)
-		if is_vuln:
-			message = '%s %s running on %s:%s is outdated' % (name, version, ip, port)
-			notify('[Vuln] %s' % message)
-			print('%s %s' % (good, message))
+    for port in master_db[ip]:
+        cpe = master_db[ip][port]["cpe"]
+        name = master_db[ip][port]["software"]
+        version = master_db[ip][port]["version"]
+        software = name
+        if use_cpe:
+            software = cpe
+        is_vuln = vulners(software, version, cpe=use_cpe)
+        if is_vuln:
+            message = "%s %s running on %s:%s is outdated" % (name, version, ip, port)
+            notify("[Vuln] %s" % message)
+            print("%s %s" % (good, message))
 
-print('%s Scan completed' % good)
+print("%s Scan completed" % good)
